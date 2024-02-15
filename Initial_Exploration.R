3be46ce5c4eda48a6b17588c28121c1c69a689ec
Sana Nanlawala <sanananlawala2026@u.northwestern.edu>
2024-02-15 17:15:30 -0600

    coomitted

---
+++ Initial_Exploration.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+library(ggplot2)
+online <- read_csv("data/online_review.csv")
+
+online_clean <- online |> 
+  mutate(
+    `Level of Education` = fct_recode(`Level of Education`,
+                          1 = School))
+
+online$`Level of Education` <- as.integer(online$`Level of Education`)
+
+levels(online$`Level of Education`)
+
+level_mapping <- c("School" = 0, "Under Graduate" = 1, "Post Graduate" = 2)
+
+online$levels_ed_num <- level_mapping[online$`Level of Education`]
+
+online |> 
+summarise(mean = mean(levels_ed_num),
+          median = median(levels_ed_num),
+          sd = sd(levels_ed_num),
+          min = min(levels_ed_num),
+          max = max(levels_ed_num),
+          mode = mode(levels_ed_num))
