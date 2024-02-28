0ffb8b514faafe944b10fc2fdd56244917a445ad
cartersampson <cartersampson2026@u.northwestern.edu>
2024-02-27 21:48:27 -0600

    made this sick histogram!

--- data/study time hours across class.R
+++ data/study time hours across class.R
@@ -1,6 +1,6 @@
 library(ggplot2)
 ggplot(online, aes(x = `Study time (Hours)`, fill = `Economic status`)) +
-  geom_histogram(binwidth = 1, color = "black", position = "identity", alpha = 0.5) +
+  geom_histogram(aes(y = after_stat(density)), binwidth = 1, color = "black", position = "identity", alpha = 0.5) +
   labs(title = "Histogram of Study Time Hours by Class", x = "Study Time Hours", y = "Frequency") +
   scale_fill_manual(values = c("Poor" = "pink", "Middle Class" = "red", "Rich" = "purple"))
 
